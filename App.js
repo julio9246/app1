d679ba67c898bb947d8a6f67b1989c84dcd4b5f2
Julio Cesar <julio9246@hotmail.com>
2019-10-08 14:59:26 -0300

    Importação Inicial

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, ButtonProperties } from 'react-native';
+
+const geraNumeroAleatorio = () => {
+
+  numero_aleatorio = Math.random();
+  
+  numero_aleatorio = Math.floor(numero_aleatorio * 10 );
+  alert( numero_aleatorio );
+ 
+
+}
+
+export default function App() { 
+  return ( 
+      
+    <View style={styles.container}>
+      <Text>Bom Negócio.com!</Text> 
+      <Button
+         title="Gerar um número randômico"
+         onPress={geraNumeroAleatorio}/>
+ 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+ 
